77c35b2532df76dc39b99d624e04a881d8836d2a
unknown <jason.wong6507@gmail.com>
2022-10-21 19:07:25 -0400

    Added the items document

--- index.js
+++ index.js
@@ -31,3 +31,14 @@ newImage('assets/tree.png', '200px', '300px')
 newImage('assets/pillar.png', '350px', '100px')
 newImage('assets/crate.png', '150px', '200px')
 newImage('assets/well.png', '500px', '425px')
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
