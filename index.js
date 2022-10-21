8cea2d132c7ea454555ccbb91191aff9e023dc0b
unknown <jason.wong6507@gmail.com>
2022-10-21 18:32:56 -0400

    First attempt at the function

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(w, x, y, z){
+    let x = document.createElement('img')
+    x.src = w
+    x.style.position='fixed'
+    x.style.left=(y)
+    x.style.bottom=(z)
+    document.body.append(x)
+
+}
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+newImage('assets/green-character.gif', 'greenCharacter', '100px', '200px')
 
 let tree =document.createElement('img')
 tree.src ='assets/pine-tree.png'
-tree.style.position='fixed'
-tree.style.left='450px'
-tree.style.bottom='200px'
-document.body.append(tree)
+// tree.style.position='fixed'
+// tree.style.left='450px'
+// tree.style.bottom='200px'
+// document.body.append(tree)
+
+newImage(tree, 450, 200)
