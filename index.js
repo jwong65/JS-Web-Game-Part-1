1e9aef1ff6df4a63bf265845185f2ce8102d1c15
unknown <jason.wong6507@gmail.com>
2022-10-21 18:42:26 -0400

    Function now works with 3 arguments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function newImage(w, x, y, z){
-    let x = document.createElement('img')
-    x.src = w
-    x.style.position='fixed'
-    x.style.left=(y)
-    x.style.bottom=(z)
-    document.body.append(x)
-
+function newImage( w, y, z){
+    let character = document.createElement('img')
+    character.src = w
+    character.style.position='fixed'
+    character.style.left=( y+'px')
+    character.style.bottom=(z+'px')
+    document.body.append(character)
+    return character
 }
 
 // let greenCharacter = document.createElement('img')
@@ -14,13 +14,13 @@ function newImage(w, x, y, z){
 // greenCharacter.style.left = '100px'
 // greenCharacter.style.bottom = '100px'
 // document.body.append(greenCharacter)
-newImage('assets/green-character.gif', 'greenCharacter', '100px', '200px')
+newImage('assets/green-character.gif', 100, 100);
 
-let tree =document.createElement('img')
-tree.src ='assets/pine-tree.png'
+// let tree =document.createElement('img')
+// tree.src ='assets/pine-tree.png'
 // tree.style.position='fixed'
 // tree.style.left='450px'
 // tree.style.bottom='200px'
 // document.body.append(tree)
 
-newImage(tree, 450, 200)
+// newImage(tree, 450, 200)
