a009e508d8e0caa6f151328a2f872d9d7becea0d
unknown <jason.wong6507@gmail.com>
2022-10-21 17:06:52 -0400

    Added the source for the tree in index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let tree =document.createElement('img')
+tree.src ='assets/pine-tree.png'
